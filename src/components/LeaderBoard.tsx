aab7cde6fe9c5d545d1e7ce525ee4eedea8ee273
Allister Grange <allistergrange@gmail.com>
2021-02-05 16:52:22 +1300

    Tidying up the leaderboard's aesthetic

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { createStyles, Grid, Link, makeStyles, Paper, Slide, Theme, Tooltip, Typography } from '@material-ui/core';
+import { Box, createStyles, Grid, Link, makeStyles, Paper, Slide, Theme, Tooltip, Typography } from '@material-ui/core';
 import { CoinData } from '../types';
+import LinkIcon from '@material-ui/icons/Link';
 
 type LeaderBoardProps = {
     posts: CoinData[],
@@ -8,7 +9,8 @@ type LeaderBoardProps = {
 }
 
 type LeaderBoardSegmentProps = {
-    message: string
+    message: string,
+    description?: string
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -22,17 +24,26 @@ const useStyles = makeStyles((theme: Theme) =>
             color: theme.palette.text.secondary,
             display: 'flex',
             flexDirection: 'row',
+            justifyContent: 'space-around'
         },
     })
 );
 
 const LeaderBoardSegment = (props: LeaderBoardSegmentProps) => {
     return (
-        <Paper style={{ padding: 10, flex: 4 }}>
-            <Typography>
-                {props.message}
+        <div style={{
+            padding: 10, flex: 4, marginLeft: 6, marginRight: 6,
+            display: 'flex', alignItems: 'center', justifyContent: 'center', maxWidth: 150
+        }}>
+            <Typography component="div" style={{ display: 'flex', flexDirection: 'row' }}>
+                <Box fontWeight="fontWeightBold" style={{ paddingRight: 2 }}>
+                    {props.message}
+                </Box>
+                <Box style={{ paddingLeft: 2 }}>
+                    {props.description}
+                </Box>
             </Typography>
-        </Paper>
+        </div>
     )
 }
 
@@ -45,26 +56,27 @@ export default function LeaderBoard({ posts, showingLeaderBoard }: LeaderBoardPr
             <Grid
                 container spacing={1}
             >
+
                 {
                     posts.map((leaderBoardEntry, idx) => {
                         return (
                             <Grid
                                 key={idx} item xl={12} xs={12} >
 
-                                <Paper className={classes.paper}
-                                    style={idx % 2 == 1 ? { backgroundColor: 'beige' } : { backgroundColor: 'white' }}>
+                                <Paper className={classes.paper} >
 
-                                    <LeaderBoardSegment message={(idx + 1).toString()} />
-                                    <LeaderBoardSegment message={leaderBoardEntry.id} />
-                                    <LeaderBoardSegment message={`r/${leaderBoardEntry.subReddit}`} />
-                                    <LeaderBoardSegment message={leaderBoardEntry.totalCost.toString()} />
+                                    <LeaderBoardSegment message={`#${(idx + 1).toString()}`} />
+                                    <LeaderBoardSegment message={leaderBoardEntry.id} description={'id'} />
+                                    <LeaderBoardSegment message={`r/${leaderBoardEntry.subReddit}`} description={'subreddit'} />
+                                    <LeaderBoardSegment message={leaderBoardEntry.totalCost.toString()} description={'points'} />
 
-
-                                    <Paper style={{ padding: 10, flex: 6 }}>
-                                        <Link href={leaderBoardEntry.permalink} rel="noopener" target="_blank">
+                                    <div style={{ padding: 10, flex: 6, maxWidth: 150, marginLeft: 6, marginRight: 6 }}>
+                                        <Link style={{ display: 'flex', justifyContent: 'center' }} href={leaderBoardEntry.permalink}
+                                            rel="noopener" target="_blank">
+                                            <LinkIcon />
                                             {leaderBoardEntry.id.length === 7 ? "view comment" : "view post"}
                                         </Link>
-                                    </Paper>
+                                    </div>
 
                                 </Paper>
 
