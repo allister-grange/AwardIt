0869770c4ce612aceb0b2f1b1a2676a5fa545f42
Allister Grange <allistergrange@gmail.com>
2021-02-09 16:34:54 +1300

    Styled the leaderboard better

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Avatar, Box, createStyles, Fade, Grid, Link, makeStyles, Paper, Slide, Theme, Tooltip, Typography } from '@material-ui/core';
 import { CoinData } from '../types';
-import LinkIcon from '@material-ui/icons/Link';
 
 type LeaderBoardProps = {
     posts: CoinData[],
@@ -15,6 +14,9 @@ type LeaderBoardSegmentProps = {
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        root: {
+            flexGrow: 1,
+        },
         textPadding: {
             paddingTop: '25px'
         },
@@ -22,9 +24,7 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: theme.spacing(2),
             textAlign: 'center',
             color: theme.palette.text.secondary,
-            display: 'flex',
-            flexDirection: 'row',
-            width: '70%',
+            marginBottom: '15px'
         },
     })
 );
@@ -32,14 +32,13 @@ const useStyles = makeStyles((theme: Theme) =>
 const LeaderBoardSegment = (props: LeaderBoardSegmentProps) => {
     return (
         <div style={{
-            padding: 10, flex: 4, marginLeft: 6, marginRight: 6,
-            display: 'flex', alignItems: 'center', justifyContent: 'center', maxWidth: 150
+            padding: 10,
         }}>
-            <Typography component="div" style={{ display: 'flex', flexDirection: 'row' }}>
-                <Box fontWeight="fontWeightBold" style={{ paddingRight: 2 }}>
+            <Typography component="div" style={{ textAlign: 'center' }}>
+                <Box fontWeight="fontWeightBold" >
                     {props.message}
                 </Box>
-                <Box style={{ paddingLeft: 2 }}>
+                <Box style={{}}>
                     {props.description}
                 </Box>
             </Typography>
@@ -53,86 +52,57 @@ export default function LeaderBoard({ posts, showingLeaderBoard }: LeaderBoardPr
 
     return (
         <Slide direction="up" in={showingLeaderBoard} timeout={1000} mountOnEnter unmountOnExit>
-            <Grid
-                container spacing={1} justify="center"
-            >
-
+            <div className={classes.root}>
                 {
-                    posts.map((leaderBoardEntry, idx) => {
+                    posts.slice(0, 9).map((leaderBoardEntry, idx) => {
                         return (
-                            <Grid
-                                style={{ display: 'flex', justifyContent: 'center' }} key={idx} item xl={12} xs={12}
-                            >
-
-                                <Paper className={classes.paper} >
-
-                                    <div>
+                            <Paper className={classes.paper}>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={1}>
                                         <LeaderBoardSegment message={`#${(idx + 1).toString()}`} />
-                                    </div>
-
-
-                                    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
-                                        <Link style={{ display: 'flex', alignItems: 'center' }} href={leaderBoardEntry.permalink}
-                                            rel="noopener" target="_blank">
-                                            {leaderBoardEntry.title.length > 150 ? leaderBoardEntry.title.slice(0 ,150) + '...' : leaderBoardEntry.title}
-                                        </Link>
-
-                                        <div style={{ display: 'flex', flexDirection: 'row', paddingTop: '15px' }}>
+                                    </Grid>
+                                    <Grid item container md={8} sm={8} xs={11} spacing={2}>
+                                        <Grid item sm>
+                                            <div style={{padding: 10}}>
+                                                <Link style={{  }} href={leaderBoardEntry.permalink}
+                                                    rel="noopener" target="_blank">
+                                                    {leaderBoardEntry.title.length > 300 ? leaderBoardEntry.title.slice(0, 300) + '...' : leaderBoardEntry.title}
+                                                </Link>
+                                            </div>
+                                        </Grid >
+                                        <Grid container justify="center" direction="row">
                                             {
                                                 leaderBoardEntry.coins?.map((coin) => (
                                                     <Tooltip
+                                                        style={{ cursor: 'pointer' }}
                                                         TransitionComponent={Fade}
                                                         TransitionProps={{ timeout: 600 }}
                                                         title={(coin.coin_price * coin.count) + " coins"}
                                                         placement="top"
                                                         aria-label="coin price"
                                                         arrow >
-                                                            <div style={{display: 'flex', flexDirection: 'row', paddingRight: '13px'}}>
-                                                        {/* <Paper className={classes.paper}> */}
+                                                        <div style={{ display: 'flex', flexDirection: 'row', paddingRight: '13px' }}>
                                                             <Typography variant="body1" gutterBottom>
                                                                 {coin.count + 'x '}
                                                             </Typography>
                                                             <Avatar style={{ height: '20px', width: '20px' }} sizes='sm' alt={coin.name + ' icon'} src={coin.icon} />
-                                                        {/* </Paper> */}
                                                         </div>
                                                     </Tooltip>
-
                                                 ))
                                             }
-                                        </div>
-                                    </div>
-
-                                    <div>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid container md={3} sm={3} xs={12} justify="center" direction="row">
                                         <LeaderBoardSegment message={leaderBoardEntry.totalCost.toString()} description={'points'} />
                                         <LeaderBoardSegment message={`r/${leaderBoardEntry.subReddit}`} description={'subreddit'} />
                                         <LeaderBoardSegment message={leaderBoardEntry.id} description={'id'} />
-                                    </div>
-                                    {/* </div> */}
-
-                                    {/* <div style={{ padding: 10, flex: 6, maxWidth: 150, marginLeft: 6, marginRight: 6 }}>
-                                    </div> */}
-
-                                </Paper>
-
-                                {/* <Tooltip
-                                    TransitionComponent={Fade}
-                                    TransitionProps={{ timeout: 600 }}
-                                    title={(leaderBoardEntry * leaderBoardEntry.count) + " leaderBoardEntrys"}
-                                    placement="top"
-                                    aria-label="leaderBoardEntry price"
-                                    arrow >
-                                    <Paper className={classes.paper}>
-                                        <Avatar alt={leaderBoardEntry.name + ' icon'} src={leaderBoardEntry.icon} />
-                                        <Typography className={classes.awardCardText} variant="body1" gutterBottom>
-                                            {leaderBoardEntry.count + 'x ' + leaderBoardEntry.name}
-                                        </Typography>
-                                    </Paper>
-                                </Tooltip> */}
-                            </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
                         )
                     })
                 }
-            </Grid>
+            </div>
         </Slide >
     );
 }
